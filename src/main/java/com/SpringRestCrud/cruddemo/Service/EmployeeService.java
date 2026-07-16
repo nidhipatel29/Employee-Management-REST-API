1bd94228c168bcbe34853f160cf0c519c5726396
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-07-16 15:15:33 -0400

    Define CRUD operations in EmployeeSerivce  interface

--- src/main/java/com/SpringRestCrud/cruddemo/Service/EmployeeService.java
+++ src/main/java/com/SpringRestCrud/cruddemo/Service/EmployeeService.java
@@ -7,5 +7,11 @@ import com.SpringRestCrud.cruddemo.Entity.Employee;
 public interface EmployeeService {
 
     public List<Employee> findAll();
+
+     Employee findById(int id);
+
+     Employee save(Employee employee);
+
+     void deleteById(int id);
     
 } 
