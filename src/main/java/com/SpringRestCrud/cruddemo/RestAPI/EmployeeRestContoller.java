99c976883549d9a338ea2a9c0822c7cf533915c7
Nidhi <nidhibenhemnatkum@loyalistcollege.com>
2026-07-17 10:48:18 -0400

    add post endpoint to create employee

--- src/main/java/com/SpringRestCrud/cruddemo/RestAPI/EmployeeRestContoller.java
+++ src/main/java/com/SpringRestCrud/cruddemo/RestAPI/EmployeeRestContoller.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -34,5 +37,13 @@ public class EmployeeRestContoller {
          Employee employee=employeeService.findById(empId);
          return employee;
     }
+
+    @PostMapping("/employee")
+    public Employee save(@RequestBody Employee employee){
+    employee.setId(0);
+    Employee dbEmployee =employeeService.save(employee);
+    return dbEmployee;
+
+    }  
     
 }
